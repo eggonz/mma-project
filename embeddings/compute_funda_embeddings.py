6bfaf1f0ed3ad2dcfe91369891baa4538496ad39
root-goksenin <Goksenin.yuksel@outlook.com>
2023-06-17 16:32:02 +0000

    Fix out of memory bug with fromage model

--- embeddings/compute_funda_embeddings.py
+++ embeddings/compute_funda_embeddings.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("../fromage")
+sys.path.append("./fromage")
 import os
 from fromage import models
 from fromage import utils
@@ -12,7 +12,7 @@ import glob
 
 
 
-def get_image_from_png(path):
+def get_image_from_jpeg(path):
   img = Image.open(path)
   img = img.resize((224, 224))
   img = img.convert('RGB')
@@ -26,9 +26,10 @@ def _compute_embeddings_from_images(args, path_to_images):
     model = models.load_fromage(model_dir)
     embeddings = {'embeddings': [], 'paths': []}
     # Open the json file
-    image_paths = glob.glob("*.png", root_dir = path_to_images)
+    image_paths = glob.glob("*.jpeg", root_dir = path_to_images)
     for image_path in image_paths:
-          p = get_image_from_png(path=image_path)
+          print(image_path)
+          p = get_image_from_jpeg(path=image_path)
           pixel_values = utils.get_pixel_values_for_model(model.model.feature_extractor, p)
           pixel_values = pixel_values.to(device=model.model.logit_scale.device, dtype=model.model.logit_scale.dtype)
           pixel_values = pixel_values[None, ...]
