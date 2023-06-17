0075f1099f4c3a082c3a9443880bbbf145558be7
root-goksenin <Goksenin.yuksel@outlook.com>
2023-06-17 16:33:11 +0000

    Implement prompt accepting to adapter

--- fromage_adapter.py
+++ fromage_adapter.py
@@ -15,7 +15,7 @@ class FromageModel:
   '''
   def __init__(self, model, path_to_embeddings:str, path_to_images: str):
     '''
-    Initialize the class with fromage model, path_to_embeddings and path_to_images. They need to be 
+    Initialize the class with fromage model, path_to_embeddings and path_to_images. They need to be
     absolute paths
     '''
     self.model = model
@@ -37,7 +37,7 @@ class FromageModel:
     emb_matrix = np.concatenate(emb_matrix, axis=0)
     assert len(path_array) == emb_matrix.shape[0], (len(path_array), emb_matrix.shape[0])
     return path_array,emb_matrix
-  
+
   def _update_embeddings_from_id(self, ids):
     '''
     Utility function to update embeddings from the  ids extracted from the dataframe.
@@ -65,7 +65,7 @@ class FromageModel:
 
   def _compute_embeddings_from_id(self, ids):
     '''
-    This method returns embedding matrix and path array to initialize the fromage models' embeddings matrix 
+    This method returns embedding matrix and path array to initialize the fromage models' embeddings matrix
     and path array.
     '''
     train_embs_data = self._update_embeddings_from_id(ids)
@@ -80,24 +80,45 @@ class FromageModel:
     assert len(path_array) == emb_matrix.shape[0], (len(path_array), emb_matrix.shape[0])
     return path_array, emb_matrix
 
+  @staticmethod
+  def _display_interleaved_outputs(model_outputs, one_img_per_ret=True):
+    for output in model_outputs:
+        if type(output) == str:
+            print(output)
+        elif type(output) == list:
+            if one_img_per_ret:
+                plt.figure(figsize=(3, 3))
+                plt.imshow(np.array(output[0]))
+            else:
+                fig, ax = plt.subplots(1, len(output), figsize=(3 * len(output), 3))
+                for i, image in enumerate(output):
+                    image = np.array(image)
+                    ax[i].imshow(image)
+                    ax[i].set_title(f'Retrieval #{i+1}')
+            plt.show()
+        elif type(output) == Image.Image:
+            plt.figure(figsize=(3, 3))
+            plt.imshow(np.array(output))
+            plt.show()
+          
   def refresh_embeddings(self):
     '''
-    Refresh the embeddings of the fromage model, this is done so that when user presses restart 
+    Refresh the embeddings of the fromage model, this is done so that when user presses restart
     fromage model starts with fresh embeddings.
     '''
     # Load the pre-computed embeddings and paths
     path_array,emb_matrix = self._load_embeddings()
-    self.model.path_array = path_array  
+    self.model.path_array = path_array
     # Normalize the embeddings
     logit_scale = self.model.logit_scale.exp()
     emb_matrix = torch.tensor(emb_matrix, dtype=logit_scale.dtype).to(logit_scale.device)
     emb_matrix = emb_matrix / emb_matrix.norm(dim=1, keepdim=True)
     emb_matrix = logit_scale * emb_matrix
     self.model.emb_matrix = emb_matrix
-  
+
   def update_embeddings(self, dataframe: pd.DataFrame):
     '''
-    Update the embeddings of the fromage model. We re-write everything because 
+    Update the embeddings of the fromage model. We re-write everything because
     it is faster due to L1 cache retrieval. This method goes through the data and computes
     the embeddings of the images again. In the future we can replace this with a method that only fetches
     the embeddings of the ids from a database because those embeddings in principle should never be different.
@@ -106,13 +127,37 @@ class FromageModel:
     assert self.model.emb_matrix != None and self.model.path_array != None, "Please initialize the embeddings first"
     # Load the pre-computed embeddings and paths
     path_array,emb_matrix = self._compute_embeddings_from_id(dataframe['id'])
-    self.model.path_array = path_array  
+    self.model.path_array = path_array
     # Normalize the embeddings
     logit_scale = self.model.logit_scale.exp()
     emb_matrix = torch.tensor(emb_matrix, dtype=logit_scale.dtype).to(logit_scale.device)
     emb_matrix = emb_matrix / emb_matrix.norm(dim=1, keepdim=True)
     emb_matrix = logit_scale * emb_matrix
     self.model.emb_matrix = emb_matrix
+
+  def prompt(self, image: PIL.Image, text:str, nr_of_retrieval: int = 1, debug = False) -> List[Union[PIL.Image, int]]:
+    '''
+    Prompt is expected to be a PIL image and a string of text. We want to adjust 
+    how many images we want to retrive with nr_of_retrieval.
+    Returns
+    --------
+        List of tuples of PIL.Image and int. PIL.Image is the retrieved image and int is the unique identifier
+        of the data.
+    '''
+    # Append the ret token to the text.
+    text = text + " [RET]"
+    prompt = [image, text]
+    # Set number of words to only generate images.
+    # This output is list of images and ints
+    output = self.model.generate_for_images_and_texts(prompt = prompt,
+                                            max_img_per_ret=nr_of_retrieval,
+                                            num_words=0)
+    if debug:
+      outputs = list(map(lambda x: x[0], output))
+      self._display_interleaved_outputs(outputs, nr_of_retrieval == 1)
+    return output
+
+
 
 if __name__ == "__main__":
     model_dir = './fromage/fromage_model/'
