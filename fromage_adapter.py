22d40c4e4f88e13d20682d0d934c05cfc177f924
Goksenin Yuksel <Goksenin.yuksel@outlook.com>
2023-06-18 14:38:17 +0000

    Add the proof of concept

--- fromage_adapter.py
+++ fromage_adapter.py
@@ -6,6 +6,13 @@ import sys
 sys.path.append("./fromage")
 from fromage import models
 from fromage import utils
+import os 
+import glob
+import pickle as pkl 
+import PIL
+from PIL import Image
+import matplotlib.pyplot as plt 
+from typing import List, Union
 
 class FromageModel:
   '''
@@ -26,15 +33,17 @@ class FromageModel:
     '''
     Utility function to load embeddings from the .pkl pre-computed funda embeddings.
     '''
-    # The embeddings path
+
+    # Load embeddings.
+    # Construct embedding matrix for nearest neighbor lookup.
     path_array = []
     emb_matrix = []
-    # These were precomputed for all funda dataset images.
     with open(self.path_to_embeddings, 'rb') as wf:
-          train_embs_data = pickle.load(wf)
+          train_embs_data = pkl.load(wf)
           path_array.extend(train_embs_data['paths'])
           emb_matrix.append(train_embs_data['embeddings'])
-    emb_matrix = np.concatenate(emb_matrix, axis=0)
+    emb_matrix = np.concatenate(emb_matrix[0], axis=0)
+    # Number of paths should be equal to number of embeddings.
     assert len(path_array) == emb_matrix.shape[0], (len(path_array), emb_matrix.shape[0])
     return path_array,emb_matrix
 
@@ -46,22 +55,23 @@ class FromageModel:
     '''
     # This is used for passing the ids data to embeddings, then we can retrive the images for the id
     # and embed them.
-    embeddings = {"embeddings": [], "paths": []}
-    # For each id, there exists a directory with images.
-    for id in ids:
-      # Path to images needs to contain a seperate folder for each id in the sub-folder
-      search_path = os.path.join(self.path_to_images, id)
-      # Glob is used here to retrieve the images
-      image_paths = glob.glob("*.png", root_dir = search_path)
+    embeddings = {'embeddings': [], 'paths': []}
+    # Open the json file
+    for id_ in ids:
+      image_paths = glob.glob(path_to_images + f"/{id_}/*.jpeg", recursive = True)
       for image_path in image_paths:
-            p = get_image_from_png(path=image_path)
-            pixel_values = utils.get_pixel_values_for_model(self.model.feature_extractor, p)
-            pixel_values = pixel_values.to(device=self.model.logit_scale.device, dtype=self.model.logit_scale.dtype)
-            pixel_values = pixel_values[None, ...]
-            visual_embs = self.model.get_visual_embs(pixel_values, mode='retrieval')
-            embeddings['embeddings'].append(visual_embs)
-            embeddings['paths'].append(image_path)
-      return embeddings
+        print(image_path)
+        p = utils.get_image_from_url(image_path)
+        with torch.no_grad():
+          pixel_values = utils.get_pixel_values_for_model(model.model.feature_extractor, p)
+          pixel_values = pixel_values.to(device=model.model.logit_scale.device, dtype=model.model.logit_scale.dtype)
+          pixel_values = pixel_values[None, ...]
+          visual_embs = model.model.get_visual_embs(pixel_values, mode='retrieval')
+        embeddings['embeddings'].append(visual_embs.float().cpu().numpy())
+        embeddings['paths'].append(image_path)
+    with open('/content/mma-project/fromage/fromage_model/embeddings/funda_sample_updated.pkl', 'wb') as f:
+          pickle.dump(embeddings, f)
+    return embeddings
 
   def _compute_embeddings_from_id(self, ids):
     '''
@@ -72,11 +82,9 @@ class FromageModel:
     path_array = []
     emb_matrix = []
     # These were precomputed for all funda dataset images.
-    with open(self.path_to_embeddings, 'rb') as wf:
-          train_embs_data = pickle.load(wf)
-          path_array.extend(train_embs_data['paths'])
-          emb_matrix.append(train_embs_data['embeddings'])
-    emb_matrix = np.concatenate(emb_matrix, axis=0)
+    path_array.extend(train_embs_data['paths'])
+    emb_matrix.append(train_embs_data['embeddings'])
+    emb_matrix = np.concatenate(emb_matrix[0], axis=0)
     assert len(path_array) == emb_matrix.shape[0], (len(path_array), emb_matrix.shape[0])
     return path_array, emb_matrix
 
@@ -110,7 +118,7 @@ class FromageModel:
     path_array,emb_matrix = self._load_embeddings()
     self.model.path_array = path_array
     # Normalize the embeddings
-    logit_scale = self.model.logit_scale.exp()
+    logit_scale = self.model.model.logit_scale.exp()
     emb_matrix = torch.tensor(emb_matrix, dtype=logit_scale.dtype).to(logit_scale.device)
     emb_matrix = emb_matrix / emb_matrix.norm(dim=1, keepdim=True)
     emb_matrix = logit_scale * emb_matrix
@@ -129,13 +137,13 @@ class FromageModel:
     path_array,emb_matrix = self._compute_embeddings_from_id(dataframe['id'])
     self.model.path_array = path_array
     # Normalize the embeddings
-    logit_scale = self.model.logit_scale.exp()
+    logit_scale = self.model.model.logit_scale.exp()
     emb_matrix = torch.tensor(emb_matrix, dtype=logit_scale.dtype).to(logit_scale.device)
     emb_matrix = emb_matrix / emb_matrix.norm(dim=1, keepdim=True)
     emb_matrix = logit_scale * emb_matrix
     self.model.emb_matrix = emb_matrix
 
-  def prompt(self, image: PIL.Image, text:str, nr_of_retrieval: int = 1, debug = False) -> List[Union[PIL.Image, int]]:
+  def prompt(self, text:str, image: Image.Image, nr_of_retrieval: int = 1, debug = False) -> List[Union[Image.Image, int]]:
     '''
     Prompt is expected to be a PIL image and a string of text. We want to adjust 
     how many images we want to retrive with nr_of_retrieval.
@@ -149,20 +157,32 @@ class FromageModel:
     prompt = [image, text]
     # Set number of words to only generate images.
     # This output is list of images and ints
-    output = self.model.generate_for_images_and_texts(prompt = prompt,
-                                            max_img_per_ret=nr_of_retrieval,
-                                            num_words=0)
+    print(prompt)
+    outputs, ids = self.model.generate_for_images_and_texts(prompts = prompt,
+                                            max_img_per_ret=nr_of_retrieval
+                                            )
+    print(outputs)
     if debug:
-      outputs = list(map(lambda x: x[0], output))
       self._display_interleaved_outputs(outputs, nr_of_retrieval == 1)
-    return output
+    returns = []
+    for output in outputs:
+      if type(output) != str:
+        returns.append(output)
+    return list(zip(returns, ids))
 
 
 
 if __name__ == "__main__":
-    model_dir = './fromage/fromage_model/'
-    model = models.load_fromage(model_dir)
-    path_to_embeddings = "./embeddings" 
-    path_to_images = "./images"
+    model_dir = '/content/mma-project/fromage/fromage_model'
+    model = models.load_fromage_for_embeddings(model_dir)
+    path_to_embeddings = "/content/mma-project/fromage/fromage_model/embeddings/funda_sample.pkl" 
+    path_to_images = "/content/mma-project/funda"
     adapter = FromageModel(model, path_to_embeddings, path_to_images)
-    print("Loaded the fromage model!")
+    print("Loaded the adapter fromage model!")
+    adapter.refresh_embeddings()
+    output = adapter.prompt("Similar images", utils.get_image_from_url("/content/mma-project/funda/42194096/image1.jpeg"))
+    print(output)
+    data_dummy_filter = data = {'id': [42194016, 42194023]}
+    adapter.update_embeddings(pd.DataFrame.from_dict(data_dummy_filter))
+    output = adapter.prompt("Similar images", utils.get_image_from_url("/content/mma-project/funda/42194096/image1.jpeg"))
+    print(output)
