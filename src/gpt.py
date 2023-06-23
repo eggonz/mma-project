3db7cd7a53c3861cb009ec2de24dbc0447353789
eggonz <egoitz.gonz@gmail.com>
2023-06-23 14:52:33 +0200

    hotfix: gpt deterministic

--- src/gpt.py
+++ src/gpt.py
@@ -4,10 +4,12 @@ import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-prompt = (
-    "In the next messages, I will send you a natural language query, "
-    "you have to transform it to a pandas query string. "
-    "My dataframe has the following columns: "
+system_prompt = (
+    "You are a natural language processing tool from natural language to a single Python string. "
+    "The user will input natural language, you will output a pandas query string. "
+    "The output must be a single string that can be directly passed to the pandas DataFrame query method (pd.DataFrame.query). "
+    "Do not answer anything that does not belong to the output string, no explanations, no comments, no variable definition, no usage examples. "
+    "The target pandas DataFrame has the following columns: "
     "city (string), "
     "price (real number indicating the house price; "
     "100000 is cheapest, 2000000 is most expensive), "
@@ -15,8 +17,6 @@ prompt = (
     "living_area_size (number of square meters with "
     "10 being smallest, 2000 being biggest), "
     "energy_label (real number with 1 being best, 13 being worst). "
-    "Please format your output in such a way that it can be directly "
-    "passed to the pandas DataFrame query method."
 )
 
 
@@ -24,11 +24,10 @@ def get_pandas_query(user_input: str):
     return openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": prompt},  # TODO try as system prompt
+            {"role": "system", "content": system_prompt},
             {"role": "user", "content": user_input},
         ],
-        temperature=0.9,
+        temperature=0.1,
     )
 
 
